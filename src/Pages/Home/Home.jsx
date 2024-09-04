a269c6302656a2f06719d39242c594b9398545d7
RickyAnchores <ricky23anchores@gmail.com>
2024-09-04 18:04:17 +0200

    Home setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home h-screen p-4 grid grid-cols-2 justify-center items-center'>
+        <div className='left text-6xl font-bold uppercase '>
+            <h1 className='title1'>If you want it to take you to a better place,</h1>
+            <h1 className='title2'>it needs to be sourced from a better place.</h1>
+        </div>
+        <div className="right"></div>
+    </div>
+  )
+}
+
+export default Home;
